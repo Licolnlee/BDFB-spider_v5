030c8e0b373bae1839c72bc28205b928a6d15db1
Licolnlee <1010833461@qq.com>
2020-01-12 19:43:18 +0800

    Cookie Autoset test

--- test1.py
+++ test1.py
@@ -109,8 +109,7 @@ def singeldownload():
         'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
         'Connection': 'keep-alive',
-        'Cookie': 'ASP.NET_SessionId=tigxfhukj3h1p5empnlhbvyb; QINGCLOUDELB=b1262d52db822794d00c3069ee5bd621ec61ed2f1b6f7d61f04556fafeaf0c45; CookieId=tigxfhukj3h1p5empnlhbvyb; CheckIPAuto=; CheckIPDate=2020-01-10 20:38:30; User_User=phone2020010610184515819; FWinCookie=1',
-        # 'DNT': '1',
+        'Cookie': 'ASP.NET_SessionId=zwebkkakbo1amu1j1brz42m0; QINGCLOUDELB=59f1d6de987b0d2fd4ddf2274d09ac70921c45dcd3b30550838de7d33d1e4651; CookieId=zwebkkakbo1amu1j1brz42m0; CheckIPAuto=; CheckIPDate=2020-01-12 16:51:24; User_User=phone2020010610184515819',
         'Host': 'www.pkulaw.cn',
         'Referer': 'https://www.pkulaw.cn/case/pfnl_a6bdb3332ec0adc4bf6da0b52d04589a8445f45b7079568dbdfb.html?match=Exact',
         # 'sec-ch-ua': 'Google Chrome 79',
@@ -124,20 +123,20 @@ def singeldownload():
         # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
         #               'Chrome/79.0.3945.88 Safari/537.36',
         # # 'User-Agent': ua.random,
-        # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,
+        #         # # 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        #         # 'Host': 'www.pkulaw.com',
+        #         # # 'Sec-Fetch-Dest':'empty',
+        #         # # 'X-Requested-With': 'XMLHttpRequest',
+        #         # 'DNT': '1',
+        #         # # 'Origin': 'https://www.pkulaw.com',
+        #         # 'Sec-Fetch-Site': 'same-origin',
+        #         # 'sec-ch-ua': 'Google Chrome 79',
+        #         # 'Sec-Fetch-Dest': 'document',application/signed-exchange;v=b3;q=0.9',
         # 'Accept-Encoding': 'gzip, deflate, br',
         # 'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
         # 'Connection': 'keep-alive',
         # # 'Content-Length': '526',
-        # # 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        # 'Host': 'www.pkulaw.com',
-        # # 'Sec-Fetch-Dest':'empty',
-        # # 'X-Requested-With': 'XMLHttpRequest',
-        # 'DNT': '1',
-        # # 'Origin': 'https://www.pkulaw.com',
-        # 'Sec-Fetch-Site': 'same-origin',
-        # 'sec-ch-ua': 'Google Chrome 79',
-        # 'Sec-Fetch-Dest': 'document',
         # 'Sec-Fetch-Mode': 'navigate',
         # 'Sec-Fetch-User': '?1',
         # 'Sec-Origin-Policy': '0',
@@ -150,7 +149,7 @@ def singeldownload():
     }
     data1 = {
         'library': 'pfnl',
-        'gid': 'a6bdb3332ec0adc4bf6da0b52d04589a8445f45b7079568dbdfb',
+        'gid': 'a6bdb3332ec0adc47f1db9bdb22905b97700c455564b7ea3bdfb',
         'type': 'txt',
         'jiamizi': ''
     }
@@ -164,9 +163,19 @@ def singeldownload():
         print(res.status_code)
         if res.status_code == 200:
             # urlretrieve(res.url, './download/test2.txt')
-            with open('./download/test2.txt', 'wb') as f:
+            with open('./download/test3.txt', 'wb') as f:
                 for chunk in res.iter_content(chunk_size = 32):  # chunk_size #设置每次下载文件的大小
                     f.write(chunk)  # 每一次循环存储一次下载下来的内容
+            with open('./download/test3.txt', 'r', encoding = 'GBK') as f:
+                lines = f.readlines()
+                first_line = lines[0]
+                # print(first_line)
+                key = "尚未登录"
+                if key in first_line:
+                    print(first_line+"请先登录获取cookie")
+                else:
+                    print('您的账号已经登陆')
+            f.close()
             print("return html....")
         else:
             print("unable to download...")
@@ -209,4 +218,10 @@ def singeldownload():
 # print(r.status_code)
 # r= requests.post(url,headers,timeout=10)
 # print(r.status_code)
-singeldownload()
+# singeldownload()
+
+def reqcookie():
+    url2 = "https://www.pkulaw.cn/case/CheckLogin/Login"
+    data2 = {
+
+    }
